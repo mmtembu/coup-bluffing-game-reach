40cc798c24e85dd0f7d7060e27e2e15b680c0a51
mmtembu <mmtembu@student.wethinkcode.co.za>
2022-03-12 20:02:18 +0200

    feat(Play and Play Again) complete that section

--- index.mjs
+++ index.mjs
@@ -18,9 +18,15 @@ const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
 
 const Player = (Who) => ({
     ...stdlib.hasRandom,
-    getHand: () => {
+    getHand: async () => {
         const hand = Math.floor(Math.random() * 3);
-        console.log(`${Who} played ${HAND[hand]}`)
+        console.log(`${Who} played ${HAND[hand]}`);
+        if(Math.random() <= 0.01){
+            for(let i = 0; i < 10; i++){
+                console.log(`${Who} takes their sweet time sending it back...`);
+                await stdlib.wait(1);
+            }
+        }
         return hand;
     },
     seeOutcome: (outcome) =>{
@@ -31,7 +37,6 @@ const Player = (Who) => ({
     },
 });
 
-
 await Promise.all([
     ctcAlice.p.Alice({
         ...Player('Alice'),
@@ -40,15 +45,8 @@ await Promise.all([
     }),
     ctcBob.p.Bob({
         ...Player('Bob'),
-        acceptWager: async (amt) =>{
-            if(Math.random() <= 0.5){
-                for(let i = 0; i < 10; i++){
-                    console.log(`Bob takes hist sweet time...`);
-                    await stdlib.wait(1);
-                }
-            }else{
-                console.log(`Bob accepts the wager of ${fmt(amt)}.`)
-            }
+        acceptWager: (amt) =>{
+            console.log(`Bob accepts the wager of ${fmt(amt)}`);
         },
     }),
 ]);
