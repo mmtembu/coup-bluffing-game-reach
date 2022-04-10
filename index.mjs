e0101bc7048757e654fa9a6f333c0881cc5d4554
mmtembu <mmtembu@student.wethinkcode.co.za>
2022-04-10 19:16:07 +0200

    Redefined the multiple players array.

--- index.mjs
+++ index.mjs
@@ -1,12 +1,16 @@
 import { loadStdlib, ask } from '@reach-sh/stdlib'; 
-import * as backend from './build/index.default.mjs';
+import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib();
 
+
+let numberOfPlayersCanJoinGame = 6;
 let players = [0, 0, 0]
 let index = 0;
 
 // console.log(`Show me what is happening with this thing here: ${players}`
 
+const startingBalance = stdlib.parseCurrency(1000);
+// const fmt = (x) => stdlib.formatCurrency(x, 4);
 
 // players.forEach(async (arr, f)  =>{
 //     players[f] = await ask.ask(
@@ -17,19 +21,58 @@ let index = 0;
 // });
 
 //get player name
-const player = await ask.ask(
-    `What is your name player...?`,
-    (x => x)
+
+const isFunder = await ask.ask(
+    `Are you the funder ?`,
+    ask.yesno
 )
+
+var  numberOfPlayersInGame = 0;
+var player = 0;
+
+if(isFunder){
+    player = 1;
+    numberOfPlayersInGame = Number(await ask.ask(
+        `How many players do you want in the game ?`,
+        (num => num)
+    ))
+}else{
+    player = Number(await ask.ask(
+        `What is your number player...?`,
+        (x => x)
+    ))
+}
+
 
 let acc = null;
 const createAcc = await ask.ask(
-    `Would you like to create an account? (only possible on devnet)?`.
+    `Would you like to create an account? (only possible on devnet)?`,
     ask.yesno
 );
 
 if(createAcc){
-    acc = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+    // acc = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+    // console.log(acc)
+
+    var accFunder = await stdlib.newTestAccounts(startingBalance)
+
+    // const accPlayer_arr = await Promise.all(
+    //     Array.from({ length: numberOfPlayersInGame}, ()=> 
+    //       stdlib.newTestAccount(startingBalance)
+    //     )
+    //   );
+
+    var ctcFunder = accFunder.contract(backend);
+    var ctcInfo = ctcFunder.getInfo();
+
+    var [funderAcc, ...accPlayer_arr ] = 
+    await stdlib.newTestAccounts(numberOfPlayersInGame + 1, startingBalance);
+        funderAcc.setDebugLabel('Funder');
+        accPlayer_arr.forEach((acc, idx) => acc.setDebugLabel(`Player${idx}`));
+        console.log("Accounts created!!!")
+        console.log(accFunder.networkAccount)
+    // console.log(`please show me the backend stuff ${getAddress()}`)
+    // console.log(backend)
 }else{
     const secret = await ask.ask(
         `What is your account secret?`,
@@ -38,15 +81,19 @@ if(createAcc){
     acc = await stdlib.newAccountFromSecret(secret);
 }
 
-let ctc = null;
-if(player !== ""){
-    ctc = acc.contract(backend);
-    ctc.getInfo().then((info) => {
-        console.log(`The contract is deployed as = ${JSON.stringify(info)}`); 
-    });
+var ctc = null;
+var ctcInfo = null;
+
+if(isFunder){
+    ctc = accFunder.contract(backend);
+    ctcInfo = ctc.getInfo();
+    // ctc.getInfo().then((info) => {
+    //     console.log(`The contract is deployed as = ${JSON.stringify(info)}`); });
+    ctcInfo.then((info) => {
+        console.log(`The contract is deployed as = ${JSON.stringify(info)}`); });
 }else{
     const info = await ask.ask(
-        `PLease paste the contract information:`,
+        `Please paste the contract information:`,
         JSON.parse
     );
     ctc = acc.contract(backend, info);
@@ -54,13 +101,63 @@ if(player !== ""){
 
 console.log(`${player} has entered the game...`);
 
-// players.forEach((player =>{
-    // players[index] = await  ask.ask(
-    //     `State your name player...`,
-    //     (x => x)
-    // );
-    // console.log(`Show me the players who have joined in ${players[index]}`)
-    // index++;
-// }))
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBalance = async () => fmt(await stdlib.balanceOf(playerOneAcc));
 
+const before = await getBalance();
+console.log(`Your balance is ${before}`);
+
+const interact = { ...stdlib.hasRandom };
+interact.playerNumber = numberOfPlayersInGame;
+
+// await Promise.all([
+//     backend.Player1(playerOneAcc, {
+
+//     })
+// ])
+interact.informTimeout = () => {
+    console.log(`There was a timeout.`);
+    process.exit(1);
+};
+  
+
+if(isFunder){
+    const amount = await ask.ask(
+        `How much do you want to wager?`,
+        stdlib.parseCurrency
+    );
+    interact.wager = amount;
+    console.log(`Show me the wager amount please: ${interact.wager}`)
+    interact.deadline = { ETH: 100, ALGO: 100, CFX: 1000 }[stdlib.connector];
+
+    await Promise.all([
+        backend.Player1(ctc, {
+            ...stdlib.hasRandom,
+            coins: 2,
+            hand: (() => ({first: 0, second: 0})),
+            // reduceWager: Fun([UInt], UInt),
+            // increaseWager: Fun([UInt], UInt),
+            informTimeout: (()=> {
+                console.log(`There was a timeout.`);
+                process.exit(1);
+            }),
+            playerNumber: 1,
+            wager: amount,
+            deadline: numberOfPlayersCanJoinGame * 75,
+        })
+    ])
+}else{
+    interact.acceptWager = async (amt) => {
+        const accepted = await ask.ask(
+          `Do you accept the wager of ${fmt(amt)}?`,
+          ask.yesno
+        );
+        if (!accepted) {
+          process.exit(0);
+        }
+      };
+}
+
+// const part = isPlayerOne ? ctc.p.Player1
+// await ctc.p.Player1(interact)
 ask.done();
--- index.rsh
+++ index.rsh
@@ -1,50 +1,158 @@
 'reach 0.1'
 
-const [isDead, DUKE, ASSASSIN, AMBASSADOR, CAPTAIN, CONTESSA] = makeEnum(5);
+const [isDead, NOTHING, DUKE, ASSASSIN, AMBASSADOR, CAPTAIN, CONTESSA] = makeEnum(6);
 const [isAction, INCOME, FOREIGN, COUP, TAX, ASSASSINATE, EXCHANGE, STEAL] = makeEnum(7);
-//Make every move an enum 
+const [isCounterAction, BLOCK_FOREIGN_AID, BLOCK_STEALING_COINS, BLOCK_ASSASSINATION] = makeEnum(3);
+//Make every move an enum
 
-const playerToLose = (playerToKill) => playerToKill % 5;
+const TOTAL_COINS = 50;
+const CARDS = 15;
+const CARD_SET = [
+    {DUKE: 3},
+    {ASSASSIN: 3},
+    {AMBASSADOR: 3},
+    {CAPTAIN: 3},
+    {CONTESSA: 3},
+]
 
-const Player = {
-    getIncome: Fun([UInt], Null),//Take 1 coin from the treasury
-    getForeignAid: Fun([UInt], Null),//Take 2 coins from the treasury
+const PlayerHand = Object({
+    first : UInt,
+    second : UInt
+});
+
+const Common = {
+    takeIncome: Fun([UInt], UInt),//Take 1 coin from the treasury
+    takeForeignAid: Fun([UInt], UInt),//Take 2 coins from the treasury
     throwCoup: Fun([UInt], UInt),//Pay 7 coins and choose Player to lose
 }
 
-const characterActions = (who) => {
+const Player = {
+    /**
+     * This interface is for when the player is done playing on their turn
+     * for reducing the wager or increasing the wager depending on how their turn
+     * went.
+     */
+    ...hasRandom,
+    coins: UInt,
+    // wager: UInt,
+    hand: Fun([], PlayerHand),
+    // reduceWager: Fun([UInt], UInt),
+    // increaseWager: Fun([UInt], UInt),
+    informTimeout: Fun([], Null),
+    playerNumber: UInt,
+}
 
-    if(who.toLower() == "duke"){
+
+const characterActions = (who) => {
+    if(who == DUKE){
         //Tax
-    }else if(who.toLower() == "assassin"){
+    }else if(who == ASSASSIN){
         //Assassin
-    }else if(who.toLower() == "ambassador"){
+    }else if(who == AMBASSADOR){
         //Exchange
-    }else if(who.toLower() == "captain"){
+    }else if(who == CAPTAIN){
         //Steal
-    }else if(who.toLower() == "contessa"){
+    }else if(who == CONTESSA){
         //No Action
     }
 }
 
-const dukeAction = () =>{
-
-    return "";
-}
-
+const player_hand = {first: NOTHING, second: NOTHING}
 
 export const main = Reach.App(() => {
-    const Player1 = Participant('Player1', {
-        ...Player,
+    const Funder = Participant('Funder', {
         wager: UInt,
+        numOfPlayers: UInt,
+    });
+    const Player1 = Participant('Player1', {
+        ...Common,
+        ...Player,
+        // wager: UInt,
+        deadline: UInt,
+        acceptWager: Fun([UInt], Null),
     });
     const Player2 = Participant('Player2', {
+        ...Common,
         ...Player,
-        wager: UInt,
+        // wager: UInt,
+        acceptWager: Fun([UInt], Null),
     });
-    const Player3 = Participant('Player3', {
-        ...Player,
-        wager: UInt,
-    });
+    // const Player3 = Participant('Player3', {
+    //     ...Common,
+    //     ...Player,
+    //     acceptWager: Fun([UInt], Null),
+    // });
+    // const Player4 = Participant('Player4', {
+    //     ...Common,
+    //     ...Player,
+    //     acceptWager: Fun([UInt], Null),
+    // });
+    // const Player5 = Participant('Player5', {
+    //     ...Common,
+    //     ...Player,
+    //     acceptWager: Fun([UInt], Null),
+    // });
+    // const Player6 = Participant('Player6', {
+    //     ...Common,
+    //     ...Player,
+    //     acceptWager: Fun([UInt], Null),
+    // });
     init();
+    
+
+    Funder.only(() => {
+        const wager = declassify(interact.wager)
+        const numOfPlayers = declassify(interact.numOfPlayers)
+    })
+
+    Funder.publish(wager, numOfPlayers)
+    commit()
+
+    // Player1.publish()
+    // commit()
+
+    Player1.only(() => {
+        interact.acceptWager(wager)
+        // const wager = declassify(interact.wager);//Line 20 has Alice declassify the wager for transmission
+        // // console.log(`please show me the wager that's stored here: ${wager}`)
+        // const deadline = declassify(interact.deadline);
+    });
+    // Player1.publish(wager, deadline)
+    // // .pay(wager)
+    // commit();
+    
+
+    // unknowable(Player2, Player1());
+    Player2.only(()=>{
+        interact.acceptWager(wager);
+    })
+    // Player1.publish(wager);
+    // // .pay(wager);
+    // commit();
+    // Player2.only(() => {
+    //     interact.acceptWager(wager)
+    // })
+    // // Player2.publish(); 
+    // Player2.pay(wager)
+
+    // var [ i ] = [ 0 ];
+    // invariant(balance() == 0);
+    // while(true){
+    //     commit();
+    //     Player1.only(() => {
+    //         const wager = declassify(interact.wager);
+    //         const ctc = declassify(interact.getCtc())
+    //     });
+    //     Player1.publish(ctc, wager);
+
+    //     // console.log(`show me the announcement please: ${ctc}`)
+    //     E.announce(i, ctc);
+    //     // [ i ] = [ i + 1 ];
+    //     continue;
+    // }
+
+    // commit();
+    // Player3.only(() => {
+    //     interact.acceptWager(wager)
+    // })
 });
